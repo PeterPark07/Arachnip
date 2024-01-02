dae24df034f6deab61a6374df7c53a9c704ddec2
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-02 23:59:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     # Get the user's IP address from the request object
-    ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
+    ip_address = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
 
     return f"IP Address logged successfully. {ip_address}"
 
